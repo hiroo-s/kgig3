f2a7b233f220042b36dbc8f35bc8b6b2558b96e6
Hiroo Shirasaki <sirasaki@iij.ad.jp>
2023-01-13 10:34:32 +0900

    basic auth for /op

--- routes/op.js
+++ routes/op.js
@@ -1,12 +1,28 @@
 var express = require('express');
+var basicAuth = require('basic-auth-connect');
 var router = express.Router();
 const db = require("./db.js");
 
 router.get('/', async function (req, res, next) {
+    let auth = basicAuth('admin', 'kgig3');
+    auth(req, res, function () {});
+
+    if (!req.user) {
+	return;
+    }
+
+    req.session.auth = req.user;
+
     res.render('op');
 });
 
 router.get('/list', async function (req, res, next) {
+    if (!req.session.auth) {
+        res.status(401);
+        res.send('{}');
+        return;
+    }
+
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.header('Expires', '-1');
     res.header('Pragma', 'no-cache');
@@ -14,5 +30,6 @@ router.get('/list', async function (req, res, next) {
     let all = await db.findAll();
     res.send(JSON.stringify(all));
 });
+
 
 module.exports = router;
